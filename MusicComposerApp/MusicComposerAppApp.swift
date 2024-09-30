8768ed3118591f88a07c2a613b9b3b662fdf5537
SumaitaSanila <sumaita.sanila94@gmail.com>
2024-09-30 23:34:49 +0200

    Initial Commit

---
+++ MusicComposerApp/MusicComposerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicComposerAppApp.swift
+//  MusicComposerApp
+//
+//  Created by Imtiaz Abedin on 30.09.24.
+//
+
+import SwiftUI
+
+@main
+struct MusicComposerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
